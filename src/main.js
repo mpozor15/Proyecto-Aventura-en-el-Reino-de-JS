529afbe644bc70d9c6ed0336c99be20617885fd6
mpozor15 <manuelpozor2004@gmail.com>
2025-12-03 11:58:00 +0100

    Animacion de batalla

--- src/main.js
+++ src/main.js
@@ -92,13 +92,29 @@ function prepararBatalla() {
     // 2. Pintamos las imágenes
     const imgJugador = document.querySelector('.combate .persona img');
     const imgEnemigo = document.querySelector('.combate .evil img');
+    
     if(imgJugador) imgJugador.src = jugador.imagen;
     if(imgEnemigo) imgEnemigo.src = enemigo.imagen;
+
+    // --- Reinicio De Animacion ---
+    
+    // Apagamos la animación
+    imgJugador.style.animation = 'none';
+    imgEnemigo.style.animation = 'none';
+
+    // Forzamos al navegador a "resetear" el motor gráfico (Reflow)
+    imgJugador.offsetHeight; 
+
+    // Encendemos la animación directamente aquí
+    imgJugador.style.animation = 'entrarIzquierda 1s cubic-bezier(0.175, 0.885, 0.32, 1.275)';
+    imgEnemigo.style.animation = 'entrarDerecha 1s cubic-bezier(0.175, 0.885, 0.32, 1.275)';
+
+    // -------------------------------------
 
     // 3. Simulamos el combate
     const resultado = simularCombate(jugador, enemigo);
 
-    // 4. Mostramos resultados (SOLO GANAR/PERDER Y PUNTOS)
+    // 4. Mostramos resultados (MINIMALISTA)
     const cajaRespuesta = document.querySelector('.respuesta');
     
     let mensajeTitulo = "";
@@ -119,7 +135,6 @@ function prepararBatalla() {
         botonTexto = "Ver Resultado Final";
     }
 
-    // Solo mostramos el título y los puntos 
     cajaRespuesta.innerHTML = `
         <h2 style="font-size: 1.5rem; margin: 10px 0; color: ${resultado.jugadorGana ? '#D9C934' : 'red'}">${mensajeTitulo}</h2>
         <p style="font-size: 1.1rem; margin: 5px 0;">Puntos ganados: <strong>${resultado.puntos}</strong></p>
